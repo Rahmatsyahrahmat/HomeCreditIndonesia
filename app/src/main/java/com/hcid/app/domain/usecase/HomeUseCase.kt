d904ab73285f8c4d07e52aec2aa28dfb454882f5
Rahmatsyahrahmat <rahmatsyah1317@gmail.com>
2020-10-13 10:28:39 +0800

    Domain layer config

--- app/src/main/java/com/hcid/app/domain/usecase/HomeUseCase.kt
+++ app/src/main/java/com/hcid/app/domain/usecase/HomeUseCase.kt
@@ -1,4 +1,12 @@
 package com.hcid.app.domain.usecase
 
+import com.hcid.app.domain.common.Resource
+import com.hcid.app.domain.entity.Article
+import com.hcid.app.domain.entity.Product
+import kotlinx.coroutines.flow.Flow
+
 interface HomeUseCase {
+    fun getArticleSectionTitle(): Flow<Resource<String>>
+    fun getListArticles(): Flow<Resource<List<Article>>>
+    fun getListProducts(): Flow<Resource<List<Product>>>
 }
--- app/src/main/java/com/hcid/app/domain/usecase/HomeUseCaseImpl.kt
+++ app/src/main/java/com/hcid/app/domain/usecase/HomeUseCaseImpl.kt
@@ -1,4 +1,19 @@
 package com.hcid.app.domain.usecase
 
-class HomeUseCaseImpl {
+import com.hcid.app.domain.common.Resource
+import com.hcid.app.domain.entity.Article
+import com.hcid.app.domain.entity.Product
+import com.hcid.app.domain.repository.HomeRepository
+import kotlinx.coroutines.flow.Flow
+
+class HomeUseCaseImpl(private val homeRepository: HomeRepository):HomeUseCase {
+    override fun getArticleSectionTitle(): Flow<Resource<String>> =
+        homeRepository.getArticleSectionTitle()
+
+    override fun getListArticles(): Flow<Resource<List<Article>>> =
+        homeRepository.getListArticles()
+
+    override fun getListProducts(): Flow<Resource<List<Product>>> =
+        homeRepository.getListProducts()
+
 }
