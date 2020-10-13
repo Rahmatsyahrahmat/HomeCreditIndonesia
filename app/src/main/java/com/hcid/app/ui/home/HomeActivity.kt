73574568749352c381bfef342e8963e8b119fac0
Rahmatsyahrahmat <rahmatsyah1317@gmail.com>
2020-10-14 01:28:43 +0800

    Home view logic config

---
+++ app/src/main/java/com/hcid/app/ui/home/HomeActivity.kt
@@ -0,0 +1,103 @@
+package com.hcid.app.ui.home
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.hcid.app.R
+import com.hcid.app.domain.common.Resource
+import com.hcid.app.ui.adapter.ArticleAdapter
+import com.hcid.app.ui.adapter.ProductAdapter
+import com.hcid.app.ui.hide
+import com.hcid.app.ui.show
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.content_main.*
+
+@AndroidEntryPoint
+class HomeActivity : AppCompatActivity() {
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    private val productAdapter:ProductAdapter by lazy {
+        ProductAdapter(this)
+    }
+    private val articleAdapter:ArticleAdapter by lazy {
+        ArticleAdapter(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        homeProductList.adapter = productAdapter
+        homeArticleList.adapter = articleAdapter
+
+        viewModel.products.observe(this){resource->
+            when(resource){
+                is Resource.Success->{
+                    resource.data?.let {
+                        productAdapter.setProducts(it)
+                    }
+                    homeProductsShimmer.hide()
+                }
+                is Resource.Loading->{
+                    if (resource.data.isNullOrEmpty()){
+                        homeProductsShimmer.show()
+                    }else{
+                        productAdapter.setProducts(resource.data)
+                        homeProductsShimmer.hide()
+                    }
+                }
+                is Resource.Error->{
+                    Toast.makeText(this,resource.message.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        viewModel.articles.observe(this){resource->
+            when(resource){
+                is Resource.Success->{
+                    resource.data?.let {
+                        articleAdapter.setArticles(it)
+                    }
+                    homeArticlesShimmer.hide()
+                }
+                is Resource.Loading->{
+                    if (resource.data.isNullOrEmpty()){
+                        homeArticlesShimmer.show()
+                    }else{
+                        articleAdapter.setArticles(resource.data)
+                        homeArticlesShimmer.hide()
+                    }
+                }
+                is Resource.Error->{
+                    Toast.makeText(this,resource.message.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        viewModel.articleSectionTitle.observe(this){resource->
+            when(resource){
+                is Resource.Success->{
+                    homeArticleSectionTitle.text = resource.data
+                    homeArticleSectionTitleShimmer.hide()
+                }
+                is Resource.Loading->{
+                    if (resource.data.isNullOrBlank()){
+                        homeArticleSectionTitleShimmer.show()
+                    }else{
+                        homeArticleSectionTitle.text = resource.data
+                        homeArticleSectionTitleShimmer.hide()
+                    }
+                }
+                is Resource.Error->{
+                    Toast.makeText(this,resource.message.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+
+}
+
